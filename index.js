91f2e3a83f70243267c6d1fe747b42d3b8d528da
r3ot <129449887+r3ot@users.noreply.github.com>
2025-07-18 15:47:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,21 @@ const modal = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
 
 modalBtn.addEventListener('click', function () {
-  modal.classList.add('open-modal'); // changed to 'show-modal'
+  modal.classList.add('open-modal'); 
 });
 
 closeBtn.addEventListener('click', function () {
-  modal.classList.remove('open-modal'); // changed to 'show-modal'
+  modal.classList.remove('open-modal'); 
+});
+
+// document.activeElement('click', e=> {
+//     if(modal.contains(e.target) && e.target !== modalBtn){
+//         modal.classList.add('hide')
+//     }
+// })
+
+modal.addEventListener('click', function (e) {
+  if (e.target === modal) {
+    modal.classList.remove('show-modal');
+  }
 });
