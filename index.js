7e621960453a9cb34433b8f54015fe54fd1801d4
r3ot <129449887+r3ot@users.noreply.github.com>
2025-07-18 13:13:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// const modalBtn = document.querySelector(".btn-modal");
+// const modal = document.querySelector(".modal-overlay");
+// const closeBtn = document.querySelector(".close-btn")
+
+// modalBtn.addEventListener('click',function(){
+//     modal.classList.add('open-modal')
+// });
+// closeBtn.addEventListener('click',function(){
+//     modal.classList.remove('open-modal')
+// });
+
+const modalBtn = document.querySelector(".btn-modal");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener('click', function () {
+  modal.classList.add('open-modal'); // changed to 'show-modal'
+});
+
+closeBtn.addEventListener('click', function () {
+  modal.classList.remove('open-modal'); // changed to 'show-modal'
+});
